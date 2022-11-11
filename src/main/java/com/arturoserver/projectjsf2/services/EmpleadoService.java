a6235c092c78882875b783b64771639a7dc1948c
ArturomcReyes <marco_reus.19@outlook.com>
2022-11-10 23:55:16 -0600

    Nombre del proyecto: project-jsf2 Descripción: Agregamos mas datos al controller

--- src/main/java/com/arturoserver/projectjsf2/services/EmpleadoService.java
+++ src/main/java/com/arturoserver/projectjsf2/services/EmpleadoService.java
@@ -26,6 +26,12 @@ public class EmpleadoService {
 		Empleado empleadoIBM = new Empleado();
 		Empleado empleadoMicrosoft = new Empleado();
 		Empleado empleadoApple = new Empleado();
+		Empleado empleadoNetflix = new Empleado();
+		Empleado empleadoAmazon = new Empleado();
+		Empleado empleadoHP = new Empleado();
+		Empleado empleadoOracle = new Empleado();
+		Empleado empleadoSpotify = new Empleado();
+		Empleado empleadoDisney = new Empleado();
 		
 		empleadoIBM.setNombre("Diego");
 		empleadoIBM.setPrimerApellido("Paniagua");
@@ -45,9 +51,53 @@ public class EmpleadoService {
 		empleadoApple.setPuesto("Architect");
 		empleadoApple.setEstatus(true);
 		
+		//:::::::::::::::::::::
+		
+		empleadoNetflix.setNombre("Miguel");
+		empleadoNetflix.setPrimerApellido("Lopez");
+		empleadoNetflix.setSegundoApellido("Romero");
+		empleadoNetflix.setPuesto("Architect");
+		empleadoNetflix.setEstatus(true);
+		
+		empleadoAmazon.setNombre("Christian");
+		empleadoAmazon.setPrimerApellido("Lopez");
+		empleadoAmazon.setSegundoApellido("Romero");
+		empleadoAmazon.setPuesto("Architect");
+		empleadoAmazon.setEstatus(true);
+		
+		empleadoHP.setNombre("Henry");
+		empleadoHP.setPrimerApellido("Lopez");
+		empleadoHP.setSegundoApellido("Romero");
+		empleadoHP.setPuesto("Architect");
+		empleadoHP.setEstatus(true);
+		
+		empleadoOracle.setNombre("Manuelito");
+		empleadoOracle.setPrimerApellido("Lopez");
+		empleadoOracle.setSegundoApellido("Romero");
+		empleadoOracle.setPuesto("Architect");
+		empleadoOracle.setEstatus(true);
+		
+		empleadoSpotify.setNombre("Luis Roberto");
+		empleadoSpotify.setPrimerApellido("Lopez");
+		empleadoSpotify.setSegundoApellido("Romero");
+		empleadoSpotify.setPuesto("Architect");
+		empleadoSpotify.setEstatus(true);
+		
+		empleadoDisney.setNombre("Vianey");
+		empleadoDisney.setPrimerApellido("Lopez");
+		empleadoDisney.setSegundoApellido("Romero");
+		empleadoDisney.setPuesto("Architect");
+		empleadoDisney.setEstatus(true);
+		
 		empleados.add(empleadoIBM);
 		empleados.add(empleadoMicrosoft);
 		empleados.add(empleadoApple);
+		empleados.add(empleadoNetflix);
+		empleados.add(empleadoAmazon);
+		empleados.add(empleadoHP);
+		empleados.add(empleadoOracle);
+		empleados.add(empleadoSpotify);
+		empleados.add(empleadoDisney);
 		
 		return empleados;
 	}
